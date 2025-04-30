0a86fe6ee20c1f2b9a66007654012d78f9932d44
pitfritsch <pedrofritsch98@gmail.com>
2025-04-29 22:48:17 -0300

    Using open ai to describe the image

---
+++ src/app/api/route.ts
@@ -0,0 +1,29 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: "thats my key",
+});
+
+export async function POST(req: Request) {
+  const buffer = await req.arrayBuffer(); // Get the raw binary
+  const base64Image = Buffer.from(buffer).toString("base64");
+
+  const response = await openai.responses.create({
+    model: "gpt-4.1-mini",
+    input: [
+      {
+        role: "user",
+        content: [
+          { type: "input_text", text: "what's in this image?" },
+          {
+            type: "input_image",
+            image_url: `data:image/jpeg;base64,${base64Image}`,
+            detail: "auto",
+          },
+        ],
+      },
+    ],
+  });
+
+  return Response.json({ response: response.output_text });
+}
--- src/app/content.tsx
+++ src/app/content.tsx
@@ -1,14 +1,55 @@
 "use client";
 import { FileSelector } from "@/components/FileSelector";
-import { Center } from "@mantine/core";
-import { useState } from "react";
+import { Button, Center, Stack, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 export function HomePage() {
   const [file, setFile] = useState<File | null>(null);
+  const [blob, setBlob] = useState<Blob | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [response, setResponse] = useState<string>("");
+
+  useEffect(() => {
+    if (file) {
+      setBlob(new Blob([file], { type: file.type }));
+    } else {
+      setBlob(null);
+    }
+  }, [file]);
+
+  async function fetchBlob() {
+    setResponse("");
+    setIsLoading(true);
+    try {
+      const response = await fetch("/api", {
+        method: "POST",
+        body: blob,
+        headers: {
+          "Content-Type": "image/jpeg",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      setResponse(json.response);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <Center>
-      <FileSelector file={file} onSelectFile={setFile} />
+      <Stack>
+        <FileSelector file={file} onSelectFile={setFile} />
+        <Button onClick={fetchBlob} loading={isLoading}>
+          What is this image
+        </Button>
+        <Text maw={"1000px"} ta={"center"}>
+          {response}
+        </Text>
+      </Stack>
     </Center>
   );
 }
